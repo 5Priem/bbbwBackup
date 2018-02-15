3342d2365615d6f55c08d2fe71f796f125ed7480
root <root@beaglebone.localdomain>
2018-02-15 23:13:33 +0000

    BeideSamen werken

--- beideSamen.py
+++ beideSamen.py
@@ -15,19 +15,29 @@ pwm.start(servopin,5, 60.0, 1)
 
 # add an extra parameter 1 if servo doesn't turn, 1 changes polarity
 #PWM.start(channel, duty, freq, polarity), polarity is 0 by default
+ctr=1
 
 while True:
-	angle = raw_input("Angle (0 to 180 x to exit):")
-	if angle == 'x':
-		pwm.stop(servopin)
-		pwm.cleanup()
-		break
-	elif int(angle) >= 0 and int(angle) <= 180:
-		angle_f = float(angle)
-		duty = 100 - ((angle_f/180)*dutySpan+dutyMin)
-		pwm.set_duty_cycle(servopin, duty)
-	else:
-		print("Not a good format, try again")
+	#angle = raw_input("Angle (0 to 180 x to exit):")
+	#if angle == 'x':
+	#	pwm.stop(servopin)
+	#	pwm.cleanup()
+	#	break
+	#elif int(angle) >= 0 and int(angle) <= 180:
+	#	angle_f = float(angle)
+	#	duty = 100 - ((angle_f/180)*dutySpan+dutyMin)
+	#	pwm.set_duty_cycle(servopin, duty)
+	#else:
+	#	print("Not a good format, try again")
+	ctr = ctr + 10
+	angle = ctr
+	angle_f = float(angle)
+	duty = 100 - ((angle_f/180)*dutySpan+dutyMin)
+	pwm.set_duty_cycle(servopin, duty)
+	#print("ctr: %s angle: %s duty: %s"%(ctr,angle,duty))
+	if ctr >= 179:
+		ctr=1
+	time.sleep(0.1)
 
 	valueHeelL=adc.read(heelL)
 	print("heelL: "+ str(valueHeelL))
