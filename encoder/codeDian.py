130225a57b6d9664f0f5e476175021894f2066af
root <root@beaglebone.localdomain>
2018-02-20 11:58:58 +0000

    Map insole toegevoegd 2

---
+++ encoder/codeDian.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from Adafruit_BBIO.SPI import SPI
+
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+spi=SPI(0,0)#4 busses, this is bus 0
+spi.msh=10000#Frequency
+spi.bpw=8#bits per word
+spi.cshigh=False#true means you select the chip, depends on the chip, here low means active, normally Low for IMU
+spi.threewire=False#if it is true, you just read, otherwise you also send commands
+spi.lsbfirst=False#Least significant bit first (left)
+spi.open(0,0)#open
+
+#GPIO.setup("P8_11",GPIO.OUT)
+#GPIO.output("P8_11",GPIO.HIGH)
+
+try:
+	while True:
+                res = spi.xfer2([0xFFFF,0xFFFF])#deliver two bytes
+
+                res1 = spi.readbytes(2)
+                angle=(res1[0]<<8)|res1[1]#merge leftbyte and rightbyte
+                angle1=angle&0x3FFF#move the first two bits
+                angle2=float(angle1)/16363*360
+                print("data is")
+                print(angle2)
+                time.sleep(.25)
+except KeyboardInterrupt:
+        spi.close()
