12d1cd504c428834df196c4730d42acff2b916ce
root <root@beaglebone.localdomain>
2018-03-25 15:05:51 +0000

    hey

--- suitIMU/final.py
+++ suitIMU/final.py
@@ -5,7 +5,7 @@ import os
 
 #*********SETUP*********#
 try:
-	mp=mpu9250.SL_MPU9250(0x68, 2)
+	mp=mpu9250.SL_MPU9250(0x69, 2)
 except:
 	print("First IMU (in Setup) not found")
 footLeft         = "P8_7"
@@ -14,8 +14,8 @@ ankleLeft        = "P8_9"
 ankleRight       = "P8_10"
 quadricepsLeft   = "P8_11"
 quadricepsRight  = "P8_12"
-handLeft          = "P8_14"
-handRight         = "P8_15"
+handLeft         = "P8_14"
+handRight        = "P8_15"
 wristLeft        = "P8_16"
 wristRight       = "P8_17"
 bicepsLeft       = "P8_18"
@@ -32,24 +32,24 @@ head             = "P9_41"
 print("Initialising GPIO's...")
 GPIO.setup(footLeft, GPIO.OUT)
 GPIO.setup(footRight, GPIO.OUT)
-#GPIO.setup(ankleLeft, GPIO.OUT)
-#GPIO.setup(ankleRight, GPIO.OUT)
-#GPIO.setup(quadricepsLeft, GPIO.OUT)
-#GPIO.setup(quadricepsRight, GPIO.OUT)
-#GPIO.setup(handLeft, GPIO.OUT)
-#GPIO.setup(handRight, GPIO.OUT)
-#GPIO.setup(wristLeft, GPIO.OUT)
-#GPIO.setup(wristRight, GPIO.OUT)
-#GPIO.setup(bicepsLeft, GPIO.OUT)
-#GPIO.setup(bicepsRight, GPIO.OUT)
-#
-#GPIO.setup(backLeft, GPIO.OUT)
-#GPIO.setup(backRight, GPIO.OUT)
-#GPIO.setup(backPelvis, GPIO.OUT)
-#GPIO.setup(frontSternum, GPIO.OUT)
-#GPIO.setup(frontBellybutton, GPIO.OUT)
-#GPIO.setup(frontPelvis, GPIO.OUT)
-#GPIO.setup(head, GPIO.OUT)
+GPIO.setup(ankleLeft, GPIO.OUT)
+GPIO.setup(ankleRight, GPIO.OUT)
+GPIO.setup(quadricepsLeft, GPIO.OUT)
+GPIO.setup(quadricepsRight, GPIO.OUT)
+GPIO.setup(handLeft, GPIO.OUT)
+GPIO.setup(handRight, GPIO.OUT)
+GPIO.setup(wristLeft, GPIO.OUT)
+GPIO.setup(wristRight, GPIO.OUT)
+GPIO.setup(bicepsLeft, GPIO.OUT)
+GPIO.setup(bicepsRight, GPIO.OUT)
+
+GPIO.setup(backLeft, GPIO.OUT)
+GPIO.setup(backRight, GPIO.OUT)
+GPIO.setup(backPelvis, GPIO.OUT)
+GPIO.setup(frontSternum, GPIO.OUT)
+GPIO.setup(frontBellybutton, GPIO.OUT)
+GPIO.setup(frontPelvis, GPIO.OUT)
+GPIO.setup(head, GPIO.OUT)
 
 fileName = "dataIMUSuit"
 
@@ -62,7 +62,10 @@ def resetValues():
     gz=0
     gy=0
     gz=0
-#Misschien overal ook mp=mpu9250.SL_MPU9250(Ox68, 2) schrijven, kweni of het nodig is
+
+data = open(fileName + '.txt', 'a+')
+
+#Misschien overal ook mp=mpu9250.SL_MPU9250(Ox69, 2) schrijven, kweni of het nodig is
 #Moet dus NIET, check test.py, ook in deze folder
 #*********LOOP*********#
 while True:
@@ -72,7 +75,6 @@ while True:
         GPIO.output(footLeft, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Left foot,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -84,7 +86,6 @@ while True:
         GPIO.output(footRight, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Right foot,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -96,7 +97,6 @@ while True:
         GPIO.output(ankleLeft, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Left ankle,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -108,7 +108,6 @@ while True:
         GPIO.output(ankleRight, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Right ankle,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -120,7 +119,6 @@ while True:
         GPIO.output(quadricepsLeft, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Left quadriceps,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -132,7 +130,6 @@ while True:
         GPIO.output(quadricepsRight, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Right quadriceps,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -144,7 +141,6 @@ while True:
         GPIO.output(handLeft, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Left hand,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -156,7 +152,6 @@ while True:
         GPIO.output(handRight, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Right hand,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -168,7 +163,6 @@ while True:
         GPIO.output(wristLeft, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Left wrist,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -180,7 +174,6 @@ while True:
         GPIO.output(wristRight, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Right wrist,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -192,7 +185,6 @@ while True:
         GPIO.output(bicepsLeft, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Left biceps,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -204,7 +196,6 @@ while True:
         GPIO.output(bicepsRight, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Right biceps,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -216,7 +207,6 @@ while True:
         GPIO.output(backLeft, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Back left,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -228,7 +218,6 @@ while True:
         GPIO.output(backRight, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Back right,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -240,7 +229,6 @@ while True:
         GPIO.output(backPelvis, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Back pelvis,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -252,7 +240,6 @@ while True:
         GPIO.output(frontSternum, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Front sternum,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -264,7 +251,6 @@ while True:
         GPIO.output(frontBellybutton, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Front bellybutton,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -276,7 +262,6 @@ while True:
         GPIO.output(frontPelvis, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Front pelvis,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     except:
@@ -288,11 +273,11 @@ while True:
         GPIO.output(head, GPIO.HIGH)
         ax, ay, az = mp.getAccel()
         gx, gy, gz = mp.getGyro()
-        data = open(fileName + '.txt', 'a+')
         data.write("Head,"+str(ax)+','+str(ay)+','+str(az)+','+str(gx)+','+str(gy)+','+str(gz) + '\n')
         resetValues()
     	data.close()
     except:
         print("Head failed to connect")
 
+data.close()
 GPIO.cleanup()
